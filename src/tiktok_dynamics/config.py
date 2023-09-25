359acb303a9af0aa2a25a2e77ab500a8e7f2b8ee
Anders Giovanni Møller <andersgiovanni@gmail.com>
2023-09-25 15:27:43 +0200

    Add data folders to config

--- src/tiktok_dynamics/config.py
+++ src/tiktok_dynamics/config.py
@@ -7,3 +7,9 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
 DATA_DIR = ROOT_DIR / "data"
+
+DATA_USER_DIR = DATA_DIR / "user"
+
+DATA_KEYWORD_DIR = DATA_DIR / "keyword"
+
+DATA_COMMENTS_DIR = DATA_DIR / "comments"
