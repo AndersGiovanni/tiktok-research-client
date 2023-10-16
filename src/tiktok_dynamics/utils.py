32469df694a3768440fa1961b19f4f57e07ef6e7
hrmussa <andersgiovanni@gmail.com>
2023-10-16 09:29:54 +0200

    Date generator function

--- src/tiktok_dynamics/utils.py
+++ src/tiktok_dynamics/utils.py
@@ -1,8 +1,11 @@
 """General utilities."""
 
 import json
+from datetime import datetime
+from datetime import timedelta
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any
+from typing import Dict
 from typing import Iterable
 from typing import List
 from typing import Union
@@ -20,3 +23,49 @@ def save_json(path: Path, container: Iterable[Dict[str, Any]]) -> None:
     print(f"Saving json to {path}")
     with open(path, "w", encoding="utf-8") as outfile:
         json.dump(container, outfile, ensure_ascii=False, indent=4)
+
+
+def generate_date_ranges(start_date_str: str, total_days: int) -> List[tuple[str, str]]:
+    """Generate data ranges.
+
+    Parameters
+    ----------
+    start_date_str : _type_
+        Start date in string format.
+    total_days : _type_
+        How many days to generate.
+
+    Returns
+    -------
+    List[tuple[str, str]]
+        Date ranges.
+    """
+    date_format = "%Y-%m-%d"
+    start_date = datetime.strptime(start_date_str, date_format)
+    end_date = start_date + timedelta(days=total_days)
+
+    date_ranges = []
+
+    while start_date < end_date:
+        next_date = start_date + timedelta(days=30)
+
+        if next_date > end_date:
+            next_date = end_date
+
+        date_ranges.append(
+            (
+                start_date.strftime(date_format).replace("-", ""),
+                next_date.strftime(date_format).replace("-", ""),
+            )
+        )
+
+        start_date = next_date + timedelta(days=1)
+
+    return date_ranges
+
+if __name__ == "__main__":
+
+    pass
+
+    a = generate_date_ranges("2021-01-01", 100)
+    b = 1
