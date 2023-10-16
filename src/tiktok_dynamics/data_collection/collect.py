73fce99d2fd30d0b614a7ea48fbfab8158f14800
Anders Giovanni Møller <andersgiovanni@gmail.com>
2023-10-16 10:07:41 +0200

    Search for terms and keywords

--- src/tiktok_dynamics/data_collection/collect.py
+++ src/tiktok_dynamics/data_collection/collect.py
@@ -4,17 +4,22 @@ import os
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Tuple
 from typing import Union
 
 import requests
 from dotenv import load_dotenv
 from requests.models import Response
+from tenacity import retry, stop_after_attempt, wait_fixed, before_log
 
 from tiktok_dynamics.config import DATA_DIR
+from tiktok_dynamics.utils import generate_date_ranges
 from tiktok_dynamics.utils import save_json
 
 
+# Set up logging
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -35,7 +40,7 @@ class TiktokClient:
             "Authorization": f"Bearer {access_token['access_token']}",
         }
 
-    def get_access_token(self) -> Response:
+    def get_access_token(self) -> Dict[str, str]:
         """Get access token from TikTok API.
 
         The access token is valid for 7200 seconds.
@@ -66,7 +71,98 @@ class TiktokClient:
 
         return response.json()
 
-    def get_user_info(self, username: str) -> Response:
+    def query(self, query: Dict[Any, Any], url: str) -> Dict[str, str]:
+        """Query TikTok API.
+
+        Args:
+            query (Dict[Any, Any]): Custom query. Follow the documentation from https://developers.tiktok.com/doc/research-api-specs-query-videos/ # noqa
+            url (str): TikTok API url.
+
+        Returns:
+            Response: Response from TikTok API.
+        """
+
+        headers: Dict[str, str] = self._get_headers()
+
+        response: Response = requests.post(
+            url,
+            headers=headers,
+            json=query,
+            timeout=30,
+        )
+
+        # Check if the response is successful
+        response.raise_for_status()
+
+        return response.json()
+    
+    def search(self, keywords: List[str], max_size: int = 1000) -> Dict[str, str]:
+
+        headers: Dict[str, str] = self._get_headers()
+
+        url: str = "https://open.tiktokapis.com/v2/research/video/query/?fields=id,region_code,like_count,username,video_description,music_id,comment_count,share_count,view_count,effect_ids,hashtag_names,playlist_id,voice_to_text"  # noqa
+
+        date_ranges: List[tuple[str,str]] = generate_date_ranges("20230101", 100)
+
+        query: Dict[str, Any] = {
+            "query": {
+                "and": [
+                    {
+                        "operation": "IN",
+                        "field_name": "keyword",
+                        "field_values": keywords,
+                    },
+                    {
+                        "operation": "IN",
+                        "field_name": "hashtag_name",
+                        "field_values": keywords,
+                    }
+                ]
+            },
+            "max_count": 100,
+        }
+
+
+        data: List[Dict[str, str]] = list()
+
+        for date_range in date_ranges:
+            
+            has_more_data: bool = True
+            
+            query["start_date"] = date_range[0]
+
+            query["end_date"] = date_range[1]
+
+            # Check if we have reached the max size
+            if len(data) >= max_size:
+                break
+
+            # Keep querying until there is no more data
+            while has_more_data:
+
+                response: Dict[str, str] = self.fetch_data(url, query)
+
+                data.extend(response["data"]["videos"])
+
+                has_more_data = response["data"]["has_more"]
+
+                query["cursor"] = response["data"]["cursor"]
+
+                query["search_id"] = response["data"]["search_id"]
+
+                # Check if we have reached the max size or there is no more data
+                if not has_more_data or len(data) >= max_size:
+                    del query["cursor"]
+                    del query["search_id"]
+                    break
+
+        logging.info(f"Collected {len(data)} videos.")
+
+        return data
+
+    def get_user_info(
+        self, username: str
+    ) -> Dict[str, str]:
         """Get user info from TikTok API.
 
         Args:
@@ -96,13 +192,15 @@ class TiktokClient:
         return response.json()
 
     def search_keyword(
-        self, keywords: List[str], paginate: bool = False, collect_max: int = 100
+        self, keywords: List[str], paginate: bool = False, start_date: str, total_days: int, collect_max: int = 100
     ) -> List[Dict[str, str]]:
         """Search keyword from TikTok API.
 
         Args:
             keywords (str): TikTok keyword.
             paginate (bool): Whether to paginate. Defaults to False.
+            start_date (str): Start date in YYYYMMDD format.
+            total_days (int): Total number of days to collect.
             collect_max (int): Max number of videos to collect. Defaults to 100.
 
         Returns:
@@ -111,6 +209,8 @@ class TiktokClient:
         headers: Dict[str, str] = self._get_headers()
 
         url: str = "https://open.tiktokapis.com/v2/research/video/query/?fields=id,region_code,like_count,username,video_description,music_id,comment_count,share_count,view_count,effect_ids,hashtag_names,playlist_id,voice_to_text"  # noqa
+
+        # dates: List[Tuple[str, str]] = generate_date_ranges(start_date, total_days)
 
         query: Dict[str, Any] = {
             "query": {
@@ -173,22 +273,42 @@ class TiktokClient:
             response.raise_for_status()
 
             return response.json()
+        
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(2), before=before_log(logger, logging.INFO))
+    def fetch_data(self, url: str, query: Dict[str, Any]) -> Dict[str, str]:
+        """Query TikTok API.
+
+        Args:
+            url (str): TikTok API url.
+            query (Dict[Any, Any]): Custom query. Follow the documentation from https://developers.tiktok.com/doc/research-api-specs-query-videos/ # noqa
+
+        Returns:
+            response (Dict[str, str]): Response from TikTok API.
+        """
+
+        headers: Dict[str, str] = self._get_headers()
+
+        try:
+            response: Response = requests.post(
+                url,
+                headers=headers,
+                json=query,
+                timeout=30,
+            )
+            response.raise_for_status()  # This will raise a HTTPError for bad responses (4xx and 5xx)
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None  # or however you want to handle failures
+        else:
+            return response.json()  # Assuming the response is JSON formatted
 
 
 if __name__ == "__main__":
     client = TiktokClient()
 
     # Get user info
-    user_info = client.get_user_info("joedotie")
+    terms = ['climate', 'global warming', 'climate change']
 
-    print(user_info)
-
-    # Search keyword
-    keyword_info = client.search_keyword(
-        ["Donald Trump"], paginate=True, collect_max=100
-    )
-
-    # save to json
-    save_json(DATA_DIR / "keyword_info.json", keyword_info)
+    data = client.search(terms, max_size=25)
 
     a = 1
--- src/tiktok_dynamics/utils.py
+++ src/tiktok_dynamics/utils.py
@@ -1,10 +1,15 @@
 """General utilities."""
 
 import json
+from datetime import datetime
+from datetime import timedelta
+import logging
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any
+from typing import Dict
 from typing import Iterable
 from typing import List
+from typing import Tuple
 from typing import Union
 
 
@@ -20,3 +25,40 @@ def save_json(path: Path, container: Iterable[Dict[str, Any]]) -> None:
     print(f"Saving json to {path}")
     with open(path, "w", encoding="utf-8") as outfile:
         json.dump(container, outfile, ensure_ascii=False, indent=4)
+
+
+def generate_date_ranges(start_date_str: str, total_days: int) -> List[Tuple[str, str]]:
+    """Generate date ranges for TikTok API.
+
+    Args:
+        start_date_str (str): Start date in string format.
+        total_days (int): Total number of days to collect.
+
+    Returns:
+        List[str]: List of date ranges.
+    """
+    date_format = "%Y-%m-%d"
+    start_date = datetime.strptime(start_date_str, date_format)
+    end_date = start_date + timedelta(days=total_days)
+
+    logging.info("Generating date ranges from %s to %s (%s days), with 30 days per range (max allowed by TikTok API)", # noqa
+                 start_date, end_date, total_days)
+
+    date_ranges = []
+
+    while start_date < end_date:
+        next_date = start_date + timedelta(days=30)
+
+        if next_date > end_date:
+            next_date = end_date
+
+        date_ranges.append(
+            (
+                start_date.strftime(date_format).replace("-", ""),
+                next_date.strftime(date_format).replace("-", ""),
+            )
+        )
+
+        start_date = next_date + timedelta(days=1)
+
+    return date_ranges
