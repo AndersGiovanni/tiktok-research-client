bd3a97cea93f2254a89d339b54f7b69d8e644cb9
Anders Giovanni Møller <andersgiovanni@gmail.com>
2023-10-19 10:59:55 +0200

    Add tests to utils

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 """Test cases for the __main__ module."""
+import os
+
 import pytest
 from click.testing import CliRunner
+
+from tiktok_research_client import __main__
 
 
 @pytest.fixture
@@ -12,3 +16,9 @@ def runner() -> CliRunner:
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     assert 0 == 0
+
+
+def test_environment_variable(runner: CliRunner) -> None:
+    """Test that the environment variable is set."""
+    runner.invoke(__main__.main)
+    assert "CLIENT_KEY" in os.environ
